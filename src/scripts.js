3bfb1058bc59878d5d4c5f57c7b11763f1098609
Logan Matheny <loganpaulmatheny@gmail.com>
2023-09-19 17:10:34 -0400

    Create the very basic HTML infrastructure I need to start visualizing the application

--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,30 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+import "./css/styles.css";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import "./images/turing-logo.png";
+console.log("This is the JavaScript entry file - your code begins here.");
 
+// ===== QUERY SELECTORS =====
+const form = document.querySelector(".login-form");
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// ===== GLOBAL VARIABLES =====
+var currentUser;
+var userPastCosts;
+var userUpcomingCosts;
+var userTotalCosts;
+
+// ===== EVENT LISTENERS =====
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // get the numbers out of the submission
+  // apiCall for the individual customer
+  // thenable that sets currentUser here
+  // apiCall all the bookings
+  // thenable for
+  // creating the user's rooms
+  // calculate the costs
+  console.log("test");
+});
