7f8e4209fc44c4de1d2476c99cbbf3b977fdecc1
Logan Matheny <loganpaulmatheny@gmail.com>
2023-09-22 09:26:56 -0400

    Completes getting the available bookings tests

--- test/customer-test.js
+++ test/customer-test.js
@@ -2,14 +2,17 @@ const chai = require("chai");
 const expect = chai.expect;
 
 import {
-  sampleRoomData,
-  sampleBookingsData,
-  miniSampleBookingsData,
-  roomCostObjectResult,
   currentSampleUser,
+  roomCostObjectResult,
+  sampleRoomData,
+  miniSampleBookingsData,
+  sampleBookingsData,
   userFourteenBookings,
   userFourteenBookingsMixedUp,
+  availableRoomsResult,
+  allRoomsBooked,
 } from "../data/sample-data";
+import { getAvailableBookings } from "../src/reservations";
 const {
   calculateRoomCosts,
   roomCosts,
@@ -57,7 +60,7 @@ describe("User bookings", function () {
 
   it("Should return an obect with blank arrays if no bookings are found", function () {
     let currentUser = {
-      id: 7,
+      id: 1007,
       name: "Logan Bond",
     };
     const userBookings = getUserBookings(
@@ -77,5 +80,37 @@ describe("Sort bookings", function () {
     let sortedBookings = sortBookings(userFourteenBookingsMixedUp);
 
     expect(sortedBookings).to.deep.equal(userFourteenBookings);
+  });
+});
+
+describe("Get available bookings", function () {
+  it("Should get all available bookings for a given day", function () {
+    let reservationDate = new Date(
+      "Fri Apr 21 2022 20:00:00 GMT-0400 (Eastern Daylight Time)"
+    );
+    reservationDate.setHours(reservationDate.getHours() + 4);
+
+    let availableBookings = getAvailableBookings(
+      reservationDate,
+      sampleRoomData.rooms,
+      sampleBookingsData.bookings
+    );
+
+    expect(availableBookings).to.deep.equal(availableRoomsResult);
+  });
+
+  it("Should return a blank array if no rooms are available on a certain date", function () {
+    let reservationDate = new Date(
+      "Fri Apr 21 2022 20:00:00 GMT-0400 (Eastern Daylight Time)"
+    );
+    reservationDate.setHours(reservationDate.getHours() + 4);
+
+    let availableBookings = getAvailableBookings(
+      reservationDate,
+      sampleRoomData.rooms,
+      allRoomsBooked.bookings
+    );
+
+    expect(availableBookings).to.deep.equal([]);
   });
 });
